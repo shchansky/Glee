864fcf039e478f521739f0fc8fb140c48cf26278
shchansky <gavrilenko71285@gmail.com>
2021-03-28 02:37:39 +0300

    added shop-list1

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,70 @@
 $(function () {
-    $('.top-slider__container').slick ({
+
+  $('.user-nav__btn').on('click', function() {
+    $('.shop-list1__content').toggleClass('shop-list1__content--list');
+    $('.shop-item').toggleClass('shop-item--list');
+
+  });
+
+
+  $(".shop-star").rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    spacing: "10px",
+    readOnly: true,
+  });
+
+
+  $(".filter-star").rateYo({
+    starWidth: "11px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    spacing: "6px",
+    readOnly: true,
+  });
+
+
+
+    $('.filter-price__input').ionRangeSlider({
+        type: "double",
+        prefix: "$",
+        onStart: function (data) {
+          $('.filter-price__from').text(data.from);
+          $('.filter-price__to').text(data.to);
+        },
+        onChange: function (data) {
+          $('.filter-price__from').text(data.from);
+          $('.filter-price__to').text(data.to);
+      },
+    });
+
+
+
+     $('.top-slider__container').slick ({
         dots:true,
         arrows:false,
         autoplay: true,
         autoplaySpeed: 2000,
     })
 
-    var mixer = mixitup('.products__items');
-    var mixer = mixitup('.new__items');
+
+
+    var containerEl1 = document.querySelector('[data-ref="container-1"]');
+    var containerEl2 = document.querySelector('[data-ref="container-2"]');
+   
+    var config = {
+      controls: {
+        scope: 'local'
+      }
+    };
+   
+    var mixer1 = mixitup(containerEl1, config);
+    var mixer2 = mixitup(containerEl2, config);
+
+     
+
+
+
 
 });
